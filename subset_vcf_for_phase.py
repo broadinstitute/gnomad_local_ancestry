6d27f1c5bb4a6138ff9ea471bb82e71d4be6facf
Mike Wilson <mwilson@broadinstitute.org>
2020-09-08 15:53:15 -0400

    Updated args

--- subset_vcf_for_phase.py
+++ subset_vcf_for_phase.py
@@ -1,13 +1,16 @@
-import logging as logger
+import logging
 
 import hail as hl
 
 from gnomad.sample_qc.pipeline import filter_rows_for_qc
 from gnomad.utils.filtering import subset_samples_and_variants
 
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def subset_and_export_chr_vcfs(
-    mt: hl.MatrixTable,
+    mt_path: str,
     samples_path: str,
     output_bucket: str,
     contigs: list,
@@ -17,23 +20,27 @@ def subset_and_export_chr_vcfs(
     """
     Subset a matrix table to specified samples and across specified contigs, filter
     on min_callrate of 0.9 and min_af of 0.001, then export each subsetted contig individually
-    :param mt: MatrixTable to subset from.
+    :param mt: Path to atrixTable to subset from.
     :param samples_path: Path to tsv of sample IDs with header "s".
     :param output_bucket: Path to output bucket for contig MT and VCF.
     :param contigs: List of contigs as integers.
     :param sparse: Boolean of whether source MT is sparse. Defaults to True.
     :param gt_expr: Boolean of GT expression in MT. Defaults to "LGT".
     """
+    logger.info(f"Running script on {contigs}...")
+    mt = hl.read_matrix_table(mt_path)
     for contig in contigs:
         contig = f"chr{contig}"
+        logger.info(f"Subsetting {contig}...")
         mt = hl.filter_intervals(
             mt, [hl.parse_locus_interval(contig, reference_genome="GRCh38")]
         )
         mt = subset_samples_and_variants(
-            mt, samples_path, sparse=sparse, gt_expr=gt_expr
+            mt, sample_path=samples_path, sparse=sparse, gt_expr=gt_expr
         )
 
         if sparse:
+            mt = mt.key_rows_by('locus', 'alleles')
             mt = hl.experimental.sparse_split_multi(mt, filter_changed_loci=True)
             mt = hl.experimental.densify(mt)
             mt = mt.filter_rows(hl.len(mt.alleles) > 1).drop(
@@ -48,6 +55,7 @@ def subset_and_export_chr_vcfs(
         mt = mt.checkpoint(
             f"{output_bucket}{contig}/gnomad_{contig}_dense_bia_snps.mt", overwrite=True
         )
+        logger.info(f"Subsetted {contig} to {mt.rows().count()} variants and {mt.cols().count()} samples")
         hl.export_vcf(
             mt, f"{output_bucket}{contig}/gnomad_{contig}_dense_bia_snps.vcf.bgz"
         )
@@ -57,20 +65,20 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mt", help="MatrixTable to subset from")
+    parser.add_argument("--mt-path", help="MatrixTable to subset from", required=True)
     parser.add_argument("--samples-path", help="TSV of samples")
-    parser.add_argument("--output-bucket", help="Bucket for MTs and VCFs")
-    parser.add_argument("--sparse", help="Whether MT is sparse")
-    parser.add_argument("--gt-expr", help="Ancestral pops to subset")
+    parser.add_argument("--output-bucket", help="Bucket for MTs and VCFs", required=True)
+    parser.add_argument("--sparse", help="Whether MT is sparse. Defaults to True", action="store_true")
+    parser.add_argument("--gt-expr", help="Genotype expression, typically 'LGT' is for sparse MTs while 'GT' for dense.", default="LGT")
     parser.add_argument(
-        "--contigs", nargs="?", help="comma separated integer contigs to run"
+        "--contigs", nargs="+", help="Integer contigs to run subsetting on", required=True
     )
     args = parser.parse_args()
     subset_and_export_chr_vcfs(
-        args.mt,
-        args.samples_path,
-        args.output_bucket,
-        args.contigs,
-        args.sparse,
-        args.gt_expr,
+        mt_path=args.mt_path,
+        samples_path=args.samples_path,
+        output_bucket=args.output_bucket,
+        contigs=args.contigs,
+        sparse=args.sparse,
+        gt_expr=args.gt_expr,
     )
