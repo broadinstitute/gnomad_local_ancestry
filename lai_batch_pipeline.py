098d2cbbc173154d2d10f1c9929c82bcfdeedd44
Mike Wilson <mwilson@broadinstitute.org>
2021-07-16 16:14:46 -0400

    Added RFMix

--- lai_batch_pipeline.py
+++ lai_batch_pipeline.py
@@ -17,7 +17,6 @@ logger = logging.getLogger(__name__)
 
 GCLOUD_USER_ACCOUNT = "mwilson@broadinstitute.org"
 GCLOUD_CREDENTIALS_LOCATION = "gs://seqr-mwilson-misc/"
-DOCKER_IMAGE = "gcr.io/broad-mpg-gnomad/lai_phasing:latest"
 
 
 def eagle(
@@ -38,7 +37,7 @@ def eagle(
     :param threads: The number of threads, should match the number of CPUs requested, defaults to 16
     :return: Batch job
     """
-    e = batch.new_job(name=f"Eagle-chr{contig}")
+    e = batch.new_job(name=f"Eagle - chr{contig}")
     e.cpu(threads)
     e.memory(mem)
     e.storage(storage)
@@ -58,21 +57,102 @@ def eagle(
     return e
 
 
+def rfmix(
+    batch,
+    sample_pvcf,
+    ref_pvcf,
+    contig,
+    sample_map,
+    rf_genetic_map,
+    mem="highmem",
+    storage="50G",
+    threads=8,
+    image="gcr.io/broad-mpg-gnomad/lai_rfmix:latest",
+) -> hb.Batch.new_job:
+    r = batch.new_job(name=f"RFMix - chr{contig}")
+    r.memory(mem)
+    r.storage(storage)
+    r.cpu(threads)
+   # r.depends_on(depends_on_job) #Python doesn't like this, find how to get around it
+    r.image(image)
+    r.declare_resource_group(ofile={"msp": "{root}.msp.tsv", "fb": "{root}.fb.tsv"})
+    cmd = f"""
+    ./rfmix/rfmix \
+        -f {sample_pvcf} \
+        -r {ref_pvcf} \
+        --chromosome=chr{contig} \
+        -m {sample_map} \
+        -g {rf_genetic_map} \
+        -n 5 \
+        -e 1 \
+        --reanalyze-reference \
+        -o {r.ofile}
+    """
+    r.command(cmd)
+    return r
+
+
+def tractor(batch, msp, vcf, ancs, zipped, contig, mem="standard",
+    storage="50G",
+    image="gcr.io/broad-mpg-gnomad/lai_tractor:latest",):
+    t = batch.new_job(name=f"Tractor - chr{contig}")
+    t.storage(storage)
+    t.image(image)
+    t.memory(mem)
+   
+    rg_def = {}
+    for i in range(ancs):
+        rg_def[f'vcf{i}'] = f'{{root}}.anc{i}.vcf'
+        rg_def[f'dos{i}'] = f'{{root}}.anc{i}.dosage.txt'
+        rg_def[f'ancdos{i}'] = f'{{root}}.anc{i}.hapcount.txt'
+
+    t.declare_resource_group(ofile=rg_def)
+
+    zipped="--zipped" if zipped else ""
+    cmd = f'''
+        python3 Tractor/ExtractTracts.py --msp {msp} --vcf {vcf} --num-ancs={ancs} {zipped} --output-path={t.ofile}
+        '''
+    t.command(cmd)
+    t.command(f'ls -l {t.ofile}*')
+    return t
+
+
 def run_lai(args):
     contig = args.contig
     logger.info(f"Running gnomAD LAI on chr{contig}")
     with run_batch(args, f"LAI - chr{contig}") as b:
-        vcf = b.read_input(args.sample_vcf)
         output_path = args.output_bucket
 
-        if args.ref_vcf:
-            ref_vcf = b.read_input(args.ref_vcf)
-            ref_e = eagle(b, ref_vcf, contig)
-            b.write_output(ref_e.ofile, dest=f"{output_path}eagle/chr{contig}_reference")
+        if args.run_eagle:
+            vcf = b.read_input(args.sample_vcf)
 
-        e = eagle(b, vcf, contig)
-        b.write_output(e.ofile, dest=f"{output_path}eagle/chr{contig}_amr")
+            if args.ref_vcf:
+                ref_vcf = b.read_input(args.ref_vcf)
+                ref_e = eagle(b, ref_vcf, contig)
+                b.write_output(
+                    ref_e.ofile, dest=f"{output_path}eagle/chr{contig}_reference"
+                )
 
+            e = eagle(b, vcf, contig)
+            b.write_output(
+                e.ofile, dest=f"{output_path}eagle/output/phased_chr{contig}_amr"
+            )
+
+        if args.run_rfmix:
+            sample_map = b.read_input(args.pop_sample_map)
+            genetic_map = b.read_input(args.genetic_map)
+            if args.phased_ref_vcf:
+                phased_ref_vcf = b.read_input(args.phased_ref_vcf)
+            else:
+                phased_ref_vcf = ref_e.ofile
+            if args.phased_sample_vcf:
+                phased_sample_vcf=b.read_input(args.phased_sample_vcf)
+            else:
+                phased_sample_vcf=e.ofile.vcf
+
+            r = rfmix(b, phased_sample_vcf, phased_ref_vcf, contig, sample_map, genetic_map)
+            b.write_output(r.ofile, dest=f"{output_path}rfmix/output/lai_chr20_amr")
+        
 
 if __name__ == "__main__":
     p = init_arg_parser(
@@ -81,19 +161,29 @@ if __name__ == "__main__":
         default_temp_bucket="gnomad-batch",
     )
     p.add_argument(
-        "--sample-vcf", required=True, help="Google bucket path to sample VCF to phase"
+        "--sample-vcf", required=True, help="Google bucket path to sample VCF to phase."
     )
     p.add_argument(
         "--ref-vcf",
         required=False,
-        help="Google bucket path reference VCF to phase if separate",
+        help="Google bucket path reference VCF to phase if separate.",
     )
     p.add_argument(
-        "--output-bucket", required=True, help="Google bucket path for results"
+        "--output-bucket", required=True, help="Google bucket path for results."
     )
-    p.add_argument("--rf-genetic-map", required=False, help="Genetic map for required for RFMix2")
-    p.add_argument("--contig", required=True, help="Chromosomes to run LAI on")
-    p.add_argument("--slack-channel", help="Slack channel to send job status to, needs @ for DM")
+    p.add_argument("--run-rfmix", action="store_true", help="Whether to run RFMix2.")
+    p.add_argument(
+        "--genetic-map", required=False, help="Genetic map for required for RFMix2.", default="gs://gnomad-batch/mwilson/lai/inputs/rfmix/genetic_map_hg38.txt"
+    )
+    p.add_argument("--pop-sample-map", help="Sample population mapping for RFMix2.")
+    p.add_argument("--contig", required=True, help="Chromosomes to run LAI on.")
+    p.add_argument(
+        "--slack-channel", help="Slack channel to send job status to, needs @ for DM."
+    )
+    p.add_argument("--phased-ref-vcf", help="Phased reference VCF, if supplied, will not re-run phasing.")
+    p.add_argument("--run-eagle", action="store_true", help="Whether to run eagle to phase samples.")
+    p.add_argument("--phased-sample-vcf", help="VCF of phased samples.")
+
     args = p.parse_args()
 
     if args.slack_channel:
@@ -101,3 +191,4 @@ if __name__ == "__main__":
             run_lai(args)
     else:
         run_lai(args)
+
